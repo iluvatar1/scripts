05fd9f4d97df7891b046ed2066b3853115823057
Iluvatar <wfoquendop@unal.edu.co>
2017-11-20 22:07:36 -0500

    Activated mackup backup after a lot of cleaning

--- backup-restore/backup-macosx-packages-config.sh
+++ backup-restore/backup-macosx-packages-config.sh
@@ -6,9 +6,10 @@ echo "Creating list of brew cask packages ..."
 brew cask list > BCK-brewcask_packages.txt
 echo "Done."
 
-# echo "Backing up general configurations with mackup ..."
-# mackup backup
-# echo "Done."
+echo "Backing up general configurations with mackup ..."
+mackup backup
+echo "Done."
+
 echo "It is assumed that you have backed up the dotfiles repo ..."
 
 echo "Creating list of conda packages ..."
